cd87f1202d3125a167bc46cc0d97be2fe17aca0f
Sönke Junike T60 <soenke@junike.de>
2014-09-21 12:01:36 +0200

    Parted function getMinifiedCode in multiple functions for better reading and better inheritance

--- includes/lib/minifier/src/AMinifier.php
+++ includes/lib/minifier/src/AMinifier.php
@@ -11,6 +11,12 @@ namespace Minifier;
 abstract class AMinifier
 {
 
+	/**
+	 * Replaces Whitepace before and after special Characters in $code
+	 * 
+	 * @param string $code
+	 * @return string $code
+	 */
 	abstract protected function trimWhitespaceFromSpecialCharacters($code);
 	
 	/**
@@ -22,6 +28,27 @@ abstract class AMinifier
 	 */
 	public function getMinifiedCode($code) {
 		
+		$code = $this->replaceStringsByConstants($code);
+		
+		$code = $this->replaceSingleLineComments_MultiLineComments_LineBreaks_DoubleWhitespace($code);
+		
+		$code = $this->trimWhitespaceFromSpecialCharacters($code);
+		
+		$code = $this->replaceConstantsByString($code);
+		
+		return $code;
+		/* Done Strip Single Line Comments */
+	} // end of member function minify
+
+	/**
+	 * brief description for method ReplaceStringsByConstants
+	 *
+	 * @param param_type $nameOfParam description
+	 * @return return_type
+	 * @throws NameOfClass condition
+	 */
+	private function replaceStringsByConstants($code) {
+		$matches = array();
 		/* Strip Single Line Comments */
 		$delimiter = array("DOUBLEQUOTES" => "\"","SINGLEQUOTES" => "'");
 		//Laufe über Delimiter, speichere Matches in einem Array, Ersetze Matches durch NAME-INDEX,
@@ -35,22 +62,20 @@ abstract class AMinifier
 			}
 		}
 		
-		// Erseze alle einzeiligen Kommentare
-		$code = preg_replace("@[^\\\]//.*$@m","",$code); //@todo wenn ich richtig sehe unterscheidet sich der CSS-Minifier im Moment nur vom JS-Minifier durch [^\\\]
+		$this->setTempMatchesForStrings($matches);
 		
-		
-		//remove all Linebreaks
-		$code = preg_replace("/\n/", "", $code);
-		
-		
-		// Strip multi-line comments
-		$code = preg_replace("@/\*(([^/]+)|(?<!\*)/)+\*/@", "", $code); //
-		
-		// Remove all multiple whitespaces by a single one
-		$code = preg_replace("/\s+/", " ", $code);
-		
-		$code = $this->trimWhitespaceFromSpecialCharacters($code);
-		
+		return $code;
+	} // end of member function replaceStringsByConstants
+	
+	/**
+	 * brief description for method replaceConstantsByString
+	 *
+	 * @param param_type $nameOfParam description
+	 * @return return_type
+	 * @throws NameOfClass condition
+	 */
+	private function replaceConstantsByString($code) {
+		$matches = $this->getTempMatchesForStrings();
 		//Laufe über alle zuvor gespeicherten Matches, Ersetze NAME-INDEX durch match,
 		$delimiter = array("SINGLEQUOTES" => "'","DOUBLEQUOTES" => "\"",/*,"REGEXP" => "/"*/);
 		foreach ($delimiter as $name => $d) {
@@ -62,7 +87,56 @@ abstract class AMinifier
 		}
 		
 		return $code;
-		/* Done Strip Single Line Comments */
-	} // end of member function minify
+	} // end of member function replaceConstantsByString
+	
+	/**
+	 * brief description for method replaceSingleLineComments_MultiLineComments_LineBreaks_DoubleWhitespace
+	 *
+	 * @param param_type $nameOfParam description
+	 * @return return_type
+	 * @throws NameOfClass condition
+	 */
+	private function replaceSingleLineComments_MultiLineComments_LineBreaks_DoubleWhitespace($code) {
+		// Erseze alle einzeiligen Kommentare
+		$code = preg_replace("@[^\\\]//.*$@m","",$code); //@todo wenn ich richtig sehe unterscheidet sich der CSS-Minifier im Moment nur vom JS-Minifier durch [^\\\]
 		
+		
+		//remove all Linebreaks
+		$code = preg_replace("/\n/", "", $code);
+		
+		// Strip multi-line comments
+		$code = preg_replace("@/\*(([^/]+)|(?<!\*)/)+\*/@", "", $code); //
+		
+		// Remove all multiple whitespaces by a single one
+		$code = preg_replace("/\s+/", " ", $code);
+		
+		return $code;
+	} // end of member function replaceSingleLineComments_MultiLineComments_LineBreaks_DoubleWhitespace
+	
+	
+	/** Name of $tempMatchesForStrings with getter/setter TempMatchesForStrings and of type array
+	 * briefVariableDescription
+	 * detailed description (Typsicherheit und Wertebereich durch Getter/Setter herstellen?)
+	 *
+	 * @var array
+	 */
+	private $tempMatchesForStrings;
+	
+	/**
+	 * Auto-generated getter for variable $tempMatchesForStrings
+	 *
+	 * @return array
+	 */
+	private function getTempMatchesForStrings( ) {
+		return $this->tempMatchesForStrings;
+	} // end of member function getTempMatchesForStrings
+	
+	/**
+	 * Auto-generated setter for variable $tempMatchesForStrings
+	 *
+	 * @param array $tempMatchesForStrings briefVariableDescription
+	 */
+	private function setTempMatchesForStrings( array $tempMatchesForStrings ) {
+		$this->tempMatchesForStrings = $tempMatchesForStrings;
+	} // end of member function setTempMatchesForStrings
 } // end of class AMinifier
