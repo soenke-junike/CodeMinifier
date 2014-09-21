3cda6822c97f0b92308b43d9e9079dcb9442e227
Sönke Junike T60 <soenke@junike.de>
2014-09-21 19:49:50 +0200

    Crated different Code-Minifier. This commit was lost during integration to an own project

---
+++ src/CSSMinifier.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Minifier;
+
+/**
+ * Minifier for CSS-Code
+ * 
+ * @author Sönke Junike <soenke-junike@wsp-hanseatic.de>
+ */
+class CSSMinifier
+extends AMinifier
+{
+	/**
+	 * @see \Minifier\AMinifier::trimWhitespaceFromSpecialCharacters()
+	 */
+	protected function trimWhitespaceFromSpecialCharacters($code) {
+		return preg_replace("/\s*(,|:|;|\{|\})\s*/", "$1", $code);
+	}
+	
+} // end of class CSSMinifier
