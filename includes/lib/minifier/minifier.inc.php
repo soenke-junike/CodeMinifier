0633b890da0f1a719aac4717ae2b5d37ce67b863
Sönke Junike T60 <soenke@junike.de>
2014-09-21 12:42:18 +0200

    changed Readme and ignored html-folder for doxygen Documentation
    Conflicts:
    	.gitignore

--- includes/lib/minifier/minifier.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__."/src/AMinifier.php";
-require_once __DIR__."/src/JavaScriptMinifier.php";
-require_once __DIR__."/src/PHPMinifier.php";
-require_once __DIR__."/src/CSSMinifier.php";
-require_once __DIR__."/src/JavaMinifier.php";
